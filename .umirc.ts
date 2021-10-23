2d103592ed335b77b0677d2ed1da11f33f22fadb
bobyba <mitiakoss@gmail.com>
2021-10-23 19:51:16 +0400

    fix

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   routes: [
     {
-      path: '/urban_smart_test',
+      path: '/',
       title: 'login',
       component: '@/pages/index',
       routes: [
